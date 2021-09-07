ea956b7950f0a5bc41d0ea5b7eb8bd8f5a02da5c
Oleksandr Kukharchuk <oldvers@gmail.com>
2021-09-07 16:31:23 +0300

    Moved the board configuration to the separate folder.

---
+++ src/hw/config/board.h
@@ -0,0 +1,12 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+/* SWD - SWO */
+#define SWD_SWO_PORT         GPIOB
+#define SWD_SWO_PIN          3
+
+/* LED */
+#define LED_PORT             GPIOC
+#define LED_PIN              13
+
+#endif  /* __BOARD_H__ */
